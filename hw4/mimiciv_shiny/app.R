8acc4eeebdefa2e78d815dc8fa02f221b3bda7f1
Xinrui Xiong <xinrui@Xinruis-MacBook-Air.local>
2024-03-08 23:27:39 -0800

    final of final

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -41,7 +41,11 @@ ui <- fluidPage(
                sidebarPanel(
                  selectInput(inputId = "VOI",
                              label = "Variable of interest:(may require some time)",
-                             choices = c("Race","Age","Gender", "LabMeasurements", "Vitals","LastCareUnit")),
+                             choices = c("Race", 
+                                         "Age", 
+                                         "Gender", 
+                                         "LabMeasurements", 
+                                         "Vitals","LastCareUnit")),
                  checkboxInput(inputId = "RMO",
                              label = "Remove Outliers in IQR method for measurement?",
                              value = TRUE),
@@ -60,7 +64,11 @@ ui <- fluidPage(
                  # textInput(inputId = "PID",
                  #           label = "Select a patient:",
                  #           value = "10013310"),
-                 autocomplete_input("PID", "Select a patient:(the plot loading may require some time)", as.character(mimic_icu_cohort$subject_id),"10013310", max_options = 1000)
+                 autocomplete_input("PID", 
+                                    "Select a patient:(the plot loading may require some time)",
+                                    as.character(mimic_icu_cohort$subject_id),
+                                    "10013310", 
+                                    max_options = 1000)
                ),
                mainPanel(
                  plotOutput("plot2")
@@ -76,31 +84,40 @@ server <- function(input, output) {
     if (input$VOI == "Race") {
       ggplot(mimic_icu_cohort, aes(x=race)) + 
         geom_bar(stat="count",fill="blue") +
-        labs(title = "Race distribution", x = "race", y = "count")+
+        labs(title = "Race distribution", x = "race", y = "count") +
         coord_flip()
     }
     else if (input$VOI == "Age") {
       ggplot(mimic_icu_cohort, aes(x=age_intime)) + 
         geom_histogram(binwidth = 5,fill="skyblue") +
-        labs(title = "Age distribution", x = "age", y = "count")+
+        labs(title = "Age distribution", x = "age", y = "count") +
         coord_flip()
     }
     else if (input$VOI =="Gender") {
       ggplot(mimic_icu_cohort, aes(x=gender)) + 
         geom_bar(stat="count",fill="green") +
-        labs(title = "Gender distribution", x = "gender", y = "count")+
+        labs(title = "Gender distribution", x = "gender", y = "count") +
         coord_flip()
     }
     
     
     else if (input$VOI == "LabMeasurements") {
       cohort_long1 <- mimic_icu_cohort %>%
-        pivot_longer(cols = c("White Blood Cells","Creatinine","Potassium","Glucose","Sodium","Chloride","Bicarbonate","Hematocrit"), names_to = "Variable", values_to = "Value")
+        pivot_longer(cols = 
+                       c("White Blood Cells",
+                         "Creatinine","Potassium",
+                         "Glucose","Sodium","Chloride",
+                         "Bicarbonate","Hematocrit"), 
+                     names_to = "Variable", values_to = "Value")
       if(input$RMO==TRUE){
         ggplot(cohort_long1,aes(x = Variable, y = Value)) +
           geom_boxplot(outlier.shape = NA) +
           scale_y_continuous(limits = c(0, 
-                                        max(quantile(cohort_long1$Value, 0.75,na.rm=TRUE) + 1.5 * IQR(cohort_long1$Value,na.rm=TRUE))),)+
+                                        max(quantile(
+                                          cohort_long1$Value, 0.75,na.rm=TRUE)
+                                            + 1.5 * 
+                                            IQR(cohort_long1$Value,na.rm=TRUE))
+                                        )) +
           labs(title = "Boxplot of Lab Measurements", 
                x = "Variable", 
                y = "Value")+
@@ -111,19 +128,27 @@ server <- function(input, output) {
           geom_boxplot() +
           labs(title = "Boxplot of Lab Measurements", 
                x = "Variable", 
-               y = "Value")+
+               y = "Value") +
           coord_flip()
       }
     }
     
     else if (input$VOI == "Vitals") {
       cohort_long2 <- mimic_icu_cohort %>%
-        pivot_longer(cols = c("Heart Rate","Non Invasive Blood Pressure systolic","Temperature Fahrenheit","Respiratory Rate", "Non Invasive Blood Pressure diastolic"), names_to = "Variable", values_to = "Value")
+        pivot_longer(cols = c("Heart Rate",
+                              "Non Invasive Blood Pressure systolic",
+                              "Temperature Fahrenheit","Respiratory Rate", 
+                              "Non Invasive Blood Pressure diastolic"), 
+                                names_to = "Variable", values_to = "Value")
       if(input$RMO==TRUE){
         ggplot(cohort_long2,aes(x = Variable, y = Value)) +
           geom_boxplot(outlier.shape = NA) +
           scale_y_continuous(limits = c(0, 
-                                        max(quantile(cohort_long2$Value, 0.75,na.rm=TRUE) + 1.5 * IQR(cohort_long2$Value,na.rm=TRUE))),)+
+                                        max(quantile(
+                                          cohort_long2$Value, 0.75,na.rm=TRUE) + 
+                                              1.5 * 
+                                            IQR(cohort_long2$Value,na.rm=TRUE))
+                                        )) +
           labs(title = "Boxplot of Lab Measurements", 
                x = "Variable", 
                y = "Value")+
@@ -134,14 +159,16 @@ server <- function(input, output) {
           geom_boxplot() +
           labs(title = "Boxplot of Lab Measurements", 
                x = "Variable", 
-               y = "Value")+
+               y = "Value") +
           coord_flip()
       }
     }
     else if (input$VOI == "LastCareUnit") {
       ggplot(mimic_icu_cohort, aes(x=last_careunit)) + 
-        geom_bar(stat="count",fill="blue") +
-        labs(title = "Last Care Unit distribution", x = "last_careunit", y = "count")+
+        geom_bar(stat="count", fill="blue") +
+        labs(title = "Last Care Unit distribution",
+             x = "last_careunit", 
+             y = "count") +
         coord_flip()
     } 
   })
@@ -152,47 +179,43 @@ server <- function(input, output) {
     # sid<-10013310
     
     # obtain the ADT data
-    sid_adt <- tbl(con_bq,"transfers")|>
+    sid_adt <- tbl(con_bq, "transfers") |>
       filter(subject_id == sid)
     
     # obtain the labevents data
-    sid_lab<-tbl(con_bq,"labevents")|>
+    sid_lab<-tbl(con_bq, "labevents") |>
       filter(subject_id == sid)
     
     # obtain the procedures data
-    sid_proc<-tbl(con_bq,"procedures_icd")|>
+    sid_proc<-tbl(con_bq, "procedures_icd") |>
       filter(subject_id == sid)
     
-    sid_proc_d<-tbl(con_bq,"d_icd_procedures")|>
-      semi_join(sid_proc, by="icd_version")|>
+    sid_proc_d<-tbl(con_bq, "d_icd_procedures") |>
+      semi_join(sid_proc, by="icd_version") |>
       semi_join(sid_proc, by="icd_code")
     
-    # sid_proc$type<-sid_proc_d$long_title[match(sid_proc$icd_code, sid_proc_d$icd_code)]
-    # sid_proc$chartdate <- as.POSIXct(sid_proc$chartdate)
-    # sid_proc$type<-substr(sid_proc$type,1,50)
-    sid_proc <- sid_proc %>%
-      left_join(sid_proc_d, by = "icd_code") %>%
+    sid_proc <- sid_proc |>
+      left_join(sid_proc_d, by = "icd_code") |>
       mutate(type = long_title,
              chartdate = TIMESTAMP(chartdate),
-             # type = SUBSTR(type, 1, 50)
              )
     
-    sid_diag<-tbl(con_bq,"diagnoses_icd")|>
+    sid_diag<-tbl(con_bq,"diagnoses_icd") |>
       filter(subject_id == sid)
     
-    sid_diag_d<-tbl(con_bq,"d_icd_diagnoses")|>
-      semi_join(sid_diag, by="icd_version")|>
-      semi_join(sid_diag, by="icd_code")
+    sid_diag_d<-tbl(con_bq,"d_icd_diagnoses") |>
+      semi_join(sid_diag, by = "icd_version") |>
+      semi_join(sid_diag, by = "icd_code")
       
-    sid_pat<-tbl(con_bq,"patients")|>
+    sid_pat<-tbl(con_bq,"patients") |>
       filter(subject_id == sid)
     
-    sid_adm<-tbl(con_bq,"admissions")|>
+    sid_adm<-tbl(con_bq,"admissions") |>
       filter(subject_id == sid)
     
     ggplot() +
       geom_segment(
-        data=sid_adt %>%filter(eventtype != "discharge"),
+        data=sid_adt %>% filter(eventtype != "discharge"),
         aes(
           x = intime, 
           xend = outtime, 
@@ -226,14 +249,16 @@ server <- function(input, output) {
       labs(
         x = "", 
         y = "",
-        title = str_c("Patient ", sid, ", ", pull(sid_pat,gender), ", ",pull(sid_pat,anchor_age), " years old ",unique(pull(sid_adm,race))),
-        # subtitle= str_c(sid_diag_d$long_title[match(sid_diag$icd_code, sid_diag_d$icd_code)][1],"\n",sid_diag_d$long_title[match(sid_diag$icd_code, sid_diag_d$icd_code)][2],"\n",sid_diag_d$long_title[match(sid_diag$icd_code, sid_diag_d$icd_code)][3]),
+        title = str_c("Patient ", sid,
+                      ", ", pull(sid_pat,gender), 
+                      ", ",pull(sid_pat,anchor_age), 
+                      " years old ",unique(pull(sid_adm,race))),
         shape="Procedure",
         color="Care Unit"
       )+
       guides(linewidth = FALSE,
              color=guide_legend(ncol=1),
-             shape=guide_legend(ncol=1))+
+             shape=guide_legend(ncol=1)) +
       theme(legend.position = "bottom")
   })
 
